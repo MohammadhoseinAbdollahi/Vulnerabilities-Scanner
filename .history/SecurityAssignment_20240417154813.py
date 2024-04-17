af960696b3d1a98e5187ae0d1498fc19dd6600c4
Mohmmadhosein Abdollahi <153551779+Mohiabd99@users.noreply.github.com>
2024-04-17 15:53:52 +0200

    write the attrs in the json file of each containers

---
+++ .history/SecurityAssignment_20240417154813.py
@@ -0,0 +1,22 @@
+import docker
+import json
+
+# Connect to the Docker Network
+client = docker.from_env()
+
+# Find the monitoring_network
+aim_network = client.networks.get('monitoring_network')
+# If the network was found, print the names of all containers in it
+if aim_network is not None:
+    # Print the containers connected to the network
+    print("Containers in monitoring_network:")
+    for container in aim_network.containers:
+        container_info = container.attrs
+        container_name = container_info['Name']
+        container_ipv4 = container_info['NetworkSettings']['Networks']['monitoring_network']['IPAddress']
+        print(f"Container ID: {container.id}")
+        print(f"Container Name: {container_name}")
+        print(f"Container IPv4 Address: {container_ipv4}")
+        print()  # Add a blank line for readability
+else:
+    print("Network 'monitoring_network' not found")
